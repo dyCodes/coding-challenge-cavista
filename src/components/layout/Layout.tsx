0cf73605025ce7af34b18217aa61eb2ce5d75446
dyCodes <yusufdaudu51@gmail.com>
2024-08-04 21:59:13 +0100

    feat: add useScrollToTop hook

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,10 +3,13 @@ import Header from './Header';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
 import './Layout.css';
+import useScrollToTop from '../../hooks/useScrollToTop';
 
 interface Props {}
 
 const Layout = (props: Props) => {
+	useScrollToTop();
+
 	return (
 		<div>
 			<Header />
